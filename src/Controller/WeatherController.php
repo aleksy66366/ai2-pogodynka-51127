006188e2b1cf42f9cd85ae7760522f6e1fe8cc98
aleksanderjerzyspiewak <154765755+aleksanderjerzyspiewak@users.noreply.github.com>
2024-10-24 23:41:07 +0200

    Change in pogodynka app, added database

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Repository\LocationRepository;
+use App\Repository\MeasurementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{city}/{country?}/{province?}', name: 'app_weather', requirements: ['city' => '\w+', 'country' => '\w*', 'province' => '\w*'])]
+    public function city(
+        string $city,
+        ?string $country,
+        ?string $province,
+        LocationRepository $locationRepository,
+        MeasurementRepository $repository
+    ): Response {
+        // Szukamy lokalizacji na podstawie przekazanych parametrów
+        $criteria = ['city' => $city];
+        if ($country) {
+            $criteria['country'] = $country;
+        }
+        if ($province) {
+            $criteria['province'] = $province;
+        }
+
+        $location = $locationRepository->findOneBy($criteria);
+
+        if (!$location) {
+            throw $this->createNotFoundException('Location not found for the provided city, country, and province.');
+        }
+
+        // Pobieramy pomiary dla znalezionej lokalizacji
+        $measurements = $repository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+    }
+}
---
+++ src/Entity/Location.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $country = null;
+
+    #[ORM\Column(length: 120, nullable: true)]
+    private ?string $province = null;
+
+    #[ORM\Column(length: 120)]
+    private ?string $city = null;
+
+    #[ORM\OneToMany(targetEntity: Measurement::class, mappedBy: 'location')]
+    private Collection $measurements;
+
+    public function __construct()
+    {
+        $this->measurements = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getProvince(): ?string
+    {
+        return $this->province;
+    }
+
+    public function setProvince(?string $province): static
+    {
+        $this->province = $province;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Measurement>
+     */
+    public function getMeasurements(): Collection
+    {
+        return $this->measurements;
+    }
+
+    public function addMeasurement(Measurement $measurement): static
+    {
+        if (!$this->measurements->contains($measurement)) {
+            $this->measurements->add($measurement);
+            $measurement->setLocation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMeasurement(Measurement $measurement): static
+    {
+        if ($this->measurements->removeElement($measurement)) {
+            // set the owning side to null (unless already changed)
+            if ($measurement->getLocation() === $this) {
+                $measurement->setLocation(null);
+            }
+        }
+
+        return $this;
+    }
+}
